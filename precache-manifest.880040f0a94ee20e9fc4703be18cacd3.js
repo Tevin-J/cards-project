7888707ac0e2cf1a75640a2f62ba7479413f69de
Anton <anton.revta@gmail.com>
2020-04-28 15:13:54 +0300

    Updates

---
+++ precache-manifest.880040f0a94ee20e9fc4703be18cacd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8777d5fda37061398bfdddcf423ab79f",
+    "url": "/cards-project/index.html"
+  },
+  {
+    "revision": "c3d6c3a0e10bcb6dbebf",
+    "url": "/cards-project/static/css/main.5252f9c5.chunk.css"
+  },
+  {
+    "revision": "1ba7645c6e57bbf025f1",
+    "url": "/cards-project/static/js/2.0c468f64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards-project/static/js/2.0c468f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d6c3a0e10bcb6dbebf",
+    "url": "/cards-project/static/js/main.eac274a6.chunk.js"
+  },
+  {
+    "revision": "894fd8d1662d415c4b13",
+    "url": "/cards-project/static/js/runtime-main.5e71cec1.js"
+  }
+]);
