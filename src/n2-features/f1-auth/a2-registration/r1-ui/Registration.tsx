77d3fa0a0cffa1dbe72d6e737c494f9f8ab470f9
elenapushenko <helenekisselova@gmail.com>
2020-05-01 21:21:10 +0300

    refacoring Registration.tsx

--- src/n2-features/f1-auth/a2-registration/r1-ui/Registration.tsx
+++ src/n2-features/f1-auth/a2-registration/r1-ui/Registration.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import style from './RegistrationPage.module.css'
+import Input from "../../../../n1-main/m1-ui/common/Input/Input";
+import MyLink from "../../../../n1-main/m1-ui/common/MyLink/MyLink";
+import {SIGN_IN_PAGE} from "../../../../n1-main/m1-ui/routes/Routes";
+import Button from "../../../../n1-main/m1-ui/common/Button/Button";
 
 const Registration: React.FC = () => {
     return (
+        <div>
         <div className={style.largeText}>
             Registration page
+        </div>
+            <Input placeholder={'Введите e-mail'} value='' onChange={() => {}}/>
+            <Input placeholder={'Введите пароль'} type={'password'} value='' onChange={() => {}}/>
+            <Input placeholder={'Введите пароль повторно'} type={'password'} value='' onChange={() => {}}/>
+            <Button name={'Регистрация'} onClick={() => {}}/>
+            <MyLink to={SIGN_IN_PAGE} text={'sign-in'}/>
         </div>
     )
 }
