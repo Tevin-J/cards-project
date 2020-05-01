b53c1774be43ef28ef1588e51937c91139c21924
Khamzat <xamzatseynaro@gmail.com>
2020-05-01 21:00:50 +0300

    add recoveryPage JSX

--- src/n2-features/f1-auth/a3-recovery/r1-ui/RecoveryPassword.tsx
+++ src/n2-features/f1-auth/a3-recovery/r1-ui/RecoveryPassword.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import style from './RecoveryPasswordPage.module.css'
+import style from './RecoveryPasswordPage.module.css';
+import Input from "../../../../n1-main/m1-ui/common/Input/Input";
+import Button from "../../../../n1-main/m1-ui/common/Button/Button";
+import {RECOVERY_PAGE, SIGN_IN_PAGE} from "../../../../n1-main/m1-ui/routes/Routes";
+import MyLink from "../../../../n1-main/m1-ui/common/MyLink/MyLink";
+
 
 const RecoveryPassword: React.FC = () => {
     return (
         <div className={style.largeText}>
             Recovery password page
+            <Input placeholder={'Введите e-mail'} type={'text'} onChange={()=> {}} value={''}/>
+            <Button name={'Отправить e-mail'} onClick={()=> {}}/>
+            <MyLink to={SIGN_IN_PAGE} text={'Войти'}/>
         </div>
     )
 }
