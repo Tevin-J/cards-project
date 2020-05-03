a2d67cfc5bd60520f7ec62ab9c0af210a4a7c693
elenapushenko <helenekisselova@gmail.com>
2020-05-04 00:23:25 +0300

    create local Registration state

--- src/n2-features/f1-auth/a2-registration/r1-ui/Registration.tsx
+++ src/n2-features/f1-auth/a2-registration/r1-ui/Registration.tsx
@@ -5,15 +5,23 @@ import MyLink from "../../../../n1-main/m1-ui/common/MyLink/MyLink";
 import {SIGN_IN_PAGE} from "../../../../n1-main/m1-ui/routes/Routes";
 import Button from "../../../../n1-main/m1-ui/common/Button/Button";
 
-const Registration: React.FC = () => {
+type OwnProps = {
+    email: string
+    enterEmail: (text: string) => void
+    password: string
+    enterPassword: (text: string) => void
+
+}
+type PropsType = OwnProps
+const Registration: React.FC<PropsType> = (props) => {
     return (
         <div>
         <div className={style.largeText}>
             Registration page
         </div>
-            <Input placeholder={'Введите e-mail'} value='' onChange={() => {}}/>
-            <Input placeholder={'Введите пароль'} type={'password'} value='' onChange={() => {}}/>
-            <Input placeholder={'Введите пароль повторно'} type={'password'} value='' onChange={() => {}}/>
+            <Input placeholder={'Введите e-mail'} value={props.email} onChange={props.enterEmail}/>
+            <Input placeholder={'Введите пароль'} type={'password'} value={props.password} onChange={props.enterPassword}/>
+            <Input placeholder={'Введите пароль повторно'} type={'password'} value={props.password} onChange={props.enterPassword}/>
             <Button name={'Регистрация'} onClick={() => {}}/>
             <MyLink to={SIGN_IN_PAGE} text={'sign-in'}/>
         </div>
--- src/n2-features/f1-auth/a2-registration/r1-ui/RegistrationContainer.tsx
+++ src/n2-features/f1-auth/a2-registration/r1-ui/RegistrationContainer.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import Registration from "./Registration";
 
+
 const RegistrationContainer: React.FC = () => {
+    const [email, enterEmail] = useState<string>('meow')
+    const [password, enterPassword] = useState<string>('900')
+
     return (
-        <Registration/>
+        <Registration email={email} enterEmail={enterEmail} password={password} enterPassword={enterPassword}/>
     )
 }
 export default RegistrationContainer
