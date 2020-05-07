80d876eab9bb2b5bd1a434287d8d9faf50e9ff49
Khamzat <xamzatseynaro@gmail.com>
2020-05-07 11:58:30 +0300

    add RecoveryPassword

--- src/n2-features/f1-auth/a3-recovery/r1-ui/RecoveryPassword.tsx
+++ src/n2-features/f1-auth/a3-recovery/r1-ui/RecoveryPassword.tsx
@@ -2,21 +2,38 @@ import React from "react";
 import style from './RecoveryPasswordPage.module.css';
 import Input from "../../../../n1-main/m1-ui/common/Input/Input";
 import Button from "../../../../n1-main/m1-ui/common/Button/Button";
-import {RECOVERY_PAGE, SIGN_IN_PAGE} from "../../../../n1-main/m1-ui/routes/Routes";
+import {SIGN_IN_PAGE, NEW_PASSWORD_PAGE} from "../../../../n1-main/m1-ui/routes/Routes";
 import MyLink from "../../../../n1-main/m1-ui/common/MyLink/MyLink";
+import spinner from "../../../../n1-main/m1-ui/common/assets/spinner.gif";
+
+type OwnProps = {
+    email: string
+    enterEmail: (text: string) => void
+    recoveryPass: () => void
+    isLoading: boolean
+    isSuccess: boolean
+    isError: boolean
+}
+type PropsType = OwnProps
+
+const RecoveryPassword: React.FC<PropsType> = (props) => {
 
 
-const RecoveryPassword: React.FC = () => {
     return (
         <div>
             <div className={style.largeText}>
                 Recovery password page
             </div>
+            <div className={style.spinner}>
+                {props.isLoading ? <img src={spinner} alt='spinner'/> : ''}
+            </div>
+            {props.isError || (props.email === '') ? <div className={style.smallText}>Указанный адрес не найден</div> : null}
             <div>
-                <Input placeholder={'Введите e-mail'} type={'text'} onChange={() => {
-                }} value={''}/>
-                <Button name={'Отправить'} onClick={() => {
-                }}/>
+                <Input placeholder={'Введите e-mail'} type={'text'}
+                       onChange={props.enterEmail}
+                       value={props.email} />
+                <Button name={'Отправить'} onClick={props.recoveryPass}
+                        disabled={props.isLoading}/>
                 <MyLink to={SIGN_IN_PAGE} text={'Регистрация'}/>
             </div>
         </div>
