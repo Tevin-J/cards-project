62fe259ca1edf7da373514149024a3805d421358
Khamzat <xamzatseynaro@gmail.com>
2020-05-07 11:57:02 +0300

    add RecoveryPasswordContainer

--- src/n2-features/f1-auth/a3-recovery/r1-ui/RecoveryPasswordContainer.tsx
+++ src/n2-features/f1-auth/a3-recovery/r1-ui/RecoveryPasswordContainer.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import RecoveryPassword from "./RecoveryPassword";
+import {useDispatch, useSelector} from "react-redux";
+import recoveryPassReducer, { toRecoveryPass } from "../r2-bll/recoveryPassReducer";
+import {AppStateType} from "../../../../n1-main/m2-bll/store";
+import {Redirect} from "react-router-dom";
+import {NEW_PASSWORD_PAGE, PROFILE_PAGE} from "../../../../n1-main/m1-ui/routes/Routes";
 
 const RecoveryPasswordContainer: React.FC = () => {
+
+    const [email, enterEmail] = useState<string>('');
+
+
+    const dispatch = useDispatch();
+
+    const recoveryPass = () => {
+        dispatch(toRecoveryPass(email))
+    };
+
+    const {isLoading, isSuccess, isError} = useSelector
+    ((store: AppStateType) => store.authReducer.recoveryPassReducer);
+
+    if (isSuccess) {
+        return <Redirect to={NEW_PASSWORD_PAGE}/>
+    }
+
     return (
-        <RecoveryPassword/>
+        <RecoveryPassword email={email} enterEmail={enterEmail}
+                          recoveryPass={recoveryPass}
+                          isLoading={isLoading} isSuccess={isSuccess} isError={isError}/>
     )
-}
+};
+
 export default RecoveryPasswordContainer
