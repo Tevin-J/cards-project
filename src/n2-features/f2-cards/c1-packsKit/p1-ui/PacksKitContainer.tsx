5f329c7522e58faafc5c24e01deced5bbd03673f
Anton <anton.revta@gmail.com>
2020-05-13 15:38:39 +0300

    made getPacks thunk and api request

--- src/n2-features/f2-cards/c1-packsKit/p1-ui/PacksKitContainer.tsx
+++ src/n2-features/f2-cards/c1-packsKit/p1-ui/PacksKitContainer.tsx
@@ -1,10 +1,18 @@
 import PacksKit from "./PacksKit";
-import React from "react";
-import {useSelector} from "react-redux";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../../../../n1-main/m2-bll/store";
+import {toGetPacks} from "../p2-bll/packsKitReducer";
 
 const PacksKitContainer: React.FC = () => {
     const {packsKit} = useSelector((store: AppStateType) => store.cardsReducer.packsKitReducer)
+    const {token} = useSelector((store: AppStateType) => store.authReducer.loginReducer)
+
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(toGetPacks(token))
+    }, [])
+
     return (
         <PacksKit kit={packsKit}/>
     )
--- src/n2-features/f2-cards/c1-packsKit/p3-dal/packsApi.ts
+++ src/n2-features/f2-cards/c1-packsKit/p3-dal/packsApi.ts
@@ -1,20 +1,60 @@
 import axios from 'axios'
 
 type GetPacksResponseType = {
+    cardPacks: Array<CardType>
+    cardPacksTotalCount: number
+    maxGrade: number
+    minGrade: number
+    page: number
+    pageCount: number
+    token: string
+    tokenDeathTime: number
+}
+export type CardType = {
     _id: string
+    _v: number
+    user_id: string
     name: string
     path: string
     grade: number
+    shots: number
     rating: number
+    type: string
+    created: string
+    updated: string
+}
+type AddPackResponseType = {
+    newCardsPack: CardsPackType
+    success: boolean
     token: string
+    tokenDeathTime: number
+}
+type CardsPackType = {
+    user_id: string
+    name: string
+    path: string
+    grade: number
+    shots: number
+    rating: number
+    type: string
 }
 const instance = axios.create({
     baseURL: `https://cards-nya-back.herokuapp.com/1.0/`
 })
 export const packsApi = {
-    getPacks(packName: string, token: string) {
+    getPacks(token: string) {
         return (
-            instance.get<GetPacksResponseType>(`/cards/pack`)
+            instance.get<GetPacksResponseType>(`/cards/pack?token=${token}`)
+        )
+    },
+    addPack(cardsPack: CardsPackType, token: string) {
+        return (
+            instance.post<AddPackResponseType>(`/cards/pack`,
+                {
+                    cardsPack: cardsPack,
+                    token: token
+                }
+            )
         )
     }
 }
